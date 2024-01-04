a9a00024bb8944ddb6bd62d0e2ec888b89d0ee5a
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2024-01-05 02:10:37 +0800

    init: initial commit

---
+++ server/src/main/java/com/youtube/clone/yuutube/YuutubeApplication.java
@@ -0,0 +1,13 @@
+package com.youtube.clone.yuutube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuutubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuutubeApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/youtube/clone/yuutube/YuutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtube.clone.yuutube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuutubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
